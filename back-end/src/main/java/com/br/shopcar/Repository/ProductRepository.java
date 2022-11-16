49d874baa1b9377409d89b38dee2e0bdd07a783b
João Marcelo Schneider da Silva e Souza <f0fp1417@t-systems.com.br>
2022-11-16 15:20:16 -0300

    classes traduzidas para o inglês

--- back-end/src/main/java/com/br/shopcar/Repository/ProductRepository.java
+++ back-end/src/main/java/com/br/shopcar/Repository/ProductRepository.java
@@ -1,11 +1,11 @@
 package com.br.shopcar.Repository;
 
-import com.br.shopcar.Model.PecaModelo;
+import com.br.shopcar.Model.ProductModel;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface PecaRepositorio extends CrudRepository<PecaModelo, Long> {
+public interface ProductRepository extends CrudRepository<ProductModel, Long> {
     
 }
