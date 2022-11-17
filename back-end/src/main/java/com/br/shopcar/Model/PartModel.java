deb03f60aaf5410bf897bfea9cb876da2c8e4baf
Douglas Almeida Carneiro <douglas.carneiro@t-systems.com>
2022-11-17 16:22:47 -0300

    Autopart's model

--- back-end/src/main/java/com/br/shopcar/Model/PartModel.java
+++ back-end/src/main/java/com/br/shopcar/Model/PartModel.java
@@ -1,10 +1,22 @@
 package com.br.shopcar.Model;
 
+import com.br.shopcar.enums.Automaker;
+import com.br.shopcar.enums.Condition;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Setter
+@Getter
+@NoArgsConstructor
 @Entity
-@Table(name = "parts")
+@Table(name = "auto_part")
 public class PartModel extends ProductModel {
-    
+    private Condition condition;
+    private String category;
+    private Automaker automaker;
+
 }
