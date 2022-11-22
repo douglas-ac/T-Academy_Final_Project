aa98360d360be79460d7fe9e45e4e1cce123fa53
Eula Paula Pereira <f0fp1411@t-systems.com.br>
2022-11-22 16:38:49 -0300

    [ADD] register component

--- frontend/README.md
+++ frontend/README.md
@@ -1,6 +1,6 @@
-# Frontend
+# Teste
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.2.0.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.0.0.
 
 ## Development server
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
