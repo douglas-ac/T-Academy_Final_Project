d5d64b2d67f1be775ded49545e5612b2e4723dd7
João Marcelo Schneider da Silva e Souza <f0fp1417@t-systems.com.br>
2022-11-23 14:28:57 -0300

    2022-11-23

---
+++ frontend/src/app/components/sellCar/sell-car04/sell-car04.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sell-car04',
+  templateUrl: './sell-car04.component.html',
+  styleUrls: ['./sell-car04.component.css']
+})
+export class SellCar04Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
