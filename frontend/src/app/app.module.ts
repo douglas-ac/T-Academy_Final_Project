c25dcb360fa580b02defc54e019b429f54d552f1
Eula Paula Pereira <f0fp1411@t-systems.com.br>
2022-11-23 14:21:47 -0300

    Adicionando tela de cadastro do usuário

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,8 +6,13 @@ import { AppComponent } from './app.component';
 import { CatalogoCarroComponent } from './components/catalogo-carro/catalogo-carro.component';
 import { LoginComponent } from './components/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { RegisterComponent } from './components/register/register.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatGridListModule} from '@angular/material/grid-list';
+
 
 @NgModule({
   declarations: [
@@ -20,7 +25,12 @@ import { RegisterComponent } from './components/register/register.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  register = new FormGroup({
+    fullname: new FormControl(),
+    email: new FormControl(),
+    password: new FormControl(),
+    confirmPassword: new FormControl(),
+    cpf: new FormControl(),
+    dateBirth: new FormControl(),
+    cellphone: new FormControl(),
+    zipCode: new FormControl(),
+    street: new FormControl(),
+    addressNumber: new FormControl(),
+    city: new FormControl(),
+    neighboor: new FormControl(),
+    addressComplement: new FormControl()
+  })
+  
 }
