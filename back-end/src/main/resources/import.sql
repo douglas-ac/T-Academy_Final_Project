5ac3084ee42d11ad0cc7f6d7abb69a341ae889de
Matheus Petters Bevilaqua <f0fp1421@t-systems.com.br>
2022-11-24 09:28:19 -0300

    [CHANGE] adding cars and car parts to import.sql

--- back-end/src/main/resources/import.sql
+++ back-end/src/main/resources/import.sql
@@ -7,3 +7,19 @@ INSERT INTO `announcement`(`bairro`,`cep`,`complemento`,`ddd`,`localidade`,`logr
 insert into `orders` (`discount`, `sub_total`, `user_id`) values (10, 450, 2);
 
 insert into `order_to_product` values (1, 4);
+
+INSERT INTO product_model(product_type, descricao, nome, preco, year, automaker, modelo, category, color, quilometragem) VALUES
+(1, "Ar condicionado e vidro elétrico", "Palio", 35000, 2012, 5, "Palio", "carro popular", "prata", 200645.3),
+(1, "Vidro elétrico", "Crossfox", 45000, 2010, 6, "Crossfox", "SUV", "Preto", 63213.2),
+(1, "Ar condicionado, motor 1.6", "Argo", 76000, 2016, 5, "Argo", "carro popular", "Branco", 12300),
+(1, "Carro 0km, vidro elétrico, ar condicionado, teto solar, câmbio automático", "Rav4", 180000, 2018, 8, "Rav4", "SUV", "Pérola", 0),
+(1, "Central multimídia, motor 2.0", "Charger", 120000, 2014, 8, "Charger GT", "Muscle", "Preto", 204201.5),
+(1, "Central multimídia", "Celta", 36500, 2006, 7, "Celta", "carro popular", "Vermelho", 120036.5),
+(1, "Seminovo, motor 1.6", "Gol", 62000, 2013, 6, "Gol", "carro popular", "Vermelho", 5072.3);
+
+INSERT INTO product_model(product_type, nome, descricao, preco, year, automaker, category, part_condition, brand, vehicle_type) VALUES
+(2, "cilindro auxiliar", "cilindro auxiliar embreagem chevrolet s10", 305.9, 2015, 1, "embreagem", 1, "SKF", "Carro"),
+(2, "correia dentada", "kit correia dentada renault duster", 229.9, 2016, 2, "correia", 1, "Zen", "Caminhão"),
+(2, "amortecedor", "Amortecedor fiat uno dianteiro Nakata",	 279.9, 2010, 3, "armotecedor", 1, "Nakata", "Carro"),
+(2, "amortecedor", "Amortecedor ford fiesta dianteiro Nakata", 310.9, 2016, 5, "armotecedor", 1, "Nakata", "Carro"),
+(2, "filtro ar condicionado", "filtro ar condicionado chevrolet celta", 29.9, 2017, 7, "climatização", 2, "Tecfil", "Carro");
