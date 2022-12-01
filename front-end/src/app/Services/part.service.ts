2ce6719078f061427cc8deae71d438ccc570b010
João Marcelo Schneider da Silva e Souza <f0fp1417@t-systems.com.br>
2022-12-01 14:19:36 -0300

    2022-12-01

--- front-end/src/app/Services/part.service.ts
+++ front-end/src/app/Services/part.service.ts
@@ -2,12 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-import { Part } from '../Model/Models';
+import { Part, PartClass } from '../Model/Models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PartService {
+
+  part:PartClass = new PartClass();
+  images:any;
 
   constructor(private http: HttpClient) { }
 
@@ -27,7 +30,28 @@ export class PartService {
     return this.http.put<[Part]>(`http://localhost:8082/api/v1/parts/${id}`, data)
   }
 
+  getAutomakers() {
+    return this.http.get<String[]>("http://localhost:8082/api/v1/cars/automakers")
+  }
+
   delete(id:number){
     return this.http.delete(`http://localhost:8082/api/v1/parts/${id}`)
   }
+
+  getPartPage() {
+    return this.part;
+  }
+
+  postImage(image : FormData){
+    return this.http.post<number>("http://localhost:8082/api/v1/image/upload",image)
+  }
+
+  getImage() {
+    return this.images
+  }
+
+  saveImage(image : any){
+    this.images = image
+  }
+
 }
