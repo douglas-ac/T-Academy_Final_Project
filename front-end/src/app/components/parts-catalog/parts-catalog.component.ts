cb2ebbc275f86fde2436f62d2a5fc7611518901a
martimpalmeira <martim.desenvolvedor@gmail.com>
2022-12-01 09:11:02 -0300

    Adding parts catalog

--- front-end/src/app/components/parts-catalog/parts-catalog.component.ts
+++ front-end/src/app/components/parts-catalog/parts-catalog.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AnnounceService } from 'src/app/Services/announce.service';
+import { Announce, Car, Product, Address, Part } from '../../Model/Models'
 
 @Component({
   selector: 'app-parts-catalog',
@@ -7,20 +9,42 @@ import { Component } from '@angular/core';
 })
 export class PartsCatalogComponent {
 
+  ads: Announce[] = []
+
   isBrandShow:boolean = false;
-  isColorShow:boolean = false;
+  isMontadoraShow:boolean = false;
   isCategoryShow:boolean = false;
+  isVehicleTypeShow:boolean = false;
+
+  constructor(private service: AnnounceService){
+    this.getAll()
+  }
 
   showAllBrands(){
     this.isBrandShow = !this.isBrandShow;
   }
 
-  showAllColors(){
-    this.isColorShow = !this.isColorShow;
+  showAllVehicleTypes(){
+    this.isVehicleTypeShow = !this.isVehicleTypeShow;
+  }
+
+  showAllMontadoras(){
+    this.isMontadoraShow = !this.isMontadoraShow;
   }
 
   showAllCategorys(){
     this.isCategoryShow = !this.isCategoryShow;
   }
+
+  getAll(){
+    // this.service.getAllCars().subscribe( (data: any) => console.log(data.content))
+    this.service.getAllParts().subscribe( (data: any) => this.ads = <Announce[]>data.content)
+  }
+
+  getPart(data: Product){
+    return data as Part
+  }
+
+
 
 }
