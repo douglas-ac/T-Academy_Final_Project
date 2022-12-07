3dff46d20149da5114a7f212f479b0bdcb6d9e00
Eula Paula Pereira <f0fp1411@t-systems.com.br>
2022-12-07 16:34:02 -0300

    [ADD]comment-answer file

---
+++ front-end/src/app/Services/comment-answer.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { CommentAnswerDtoClass } from '../Model/Models';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentAnswerService {
+
+  constructor(private http: HttpClient, private authService : AuthService) { }
+
+  get(id:number){
+    return this.http.get<[CommentAnswerDtoClass]>(`http://localhost:8082/api/v1/comment/${id}`)
+  }
+
+  post(data: CommentAnswerDtoClass){
+   let token = this.authService.getToken()
+   var header = new HttpHeaders({'Authorization': 'Bearer ' + token , 'Content-Type': 'application/json'});
+    return this.http.post<CommentAnswerDtoClass>("http://localhost:8082/api/v1/comment", data, { headers: header })
+  }
+
+  delete(id:number){
+    return this.http.delete(`http://localhost:8082/api/v1/comment/${id}`)
+  }
+
+}
